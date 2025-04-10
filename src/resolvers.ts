462478c58c09ecd3f34027a7d948e048f318af1c
Kartik Dhawan <kartikdhawan@ideaclan.com>
2025-04-10 22:00:48 +0530

    Pushed resolvers & schema to separate files

---
+++ src/resolvers.ts
@@ -0,0 +1,10 @@
+export const resolvers = {
+  Query: {
+    getOrders: () => {
+      return "ID-27863782-gdkdbbnbn";
+    },
+    exampleQuery: () => "This is an example RESPONSE",
+  },
+};
+
+export default resolvers;
